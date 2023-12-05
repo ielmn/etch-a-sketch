80541b2da97edc9d2d671b091e10b80e396cdc8c
manchoy <62684358+imelman@users.noreply.github.com>
2023-12-05 14:00:38 +0800

    feat: add erasure

--- app.js
+++ app.js
@@ -1,11 +1,33 @@
 const sketchPad = document.querySelector('.sketch-container');
-// const boxes = document.querySelectorAll('.grid');
 const gridSquare = document.querySelector('.box');
 const askBtn = document.querySelector('#ask-numbers');
 const clear =  document.querySelector('#clear');
+const eraseBtn = document.querySelector('#erase');
+const blackBtn = document.querySelector('#black');
 
-let changeGridColor = (e) => e.target.classList.add('toBlack');
 let gridAmount = 6;
+let currentState;
+
+function changeState(state) {
+    switch(state){
+        case 'erase':
+            currentState = 'erase'
+            break;
+        case 'black':
+            currentState = 'black'
+            break;
+        default:
+            currentState = 'black';
+    }
+}
+
+let changeGridColor = (e) => {
+    if (currentState == 'erase'){
+        e.target.style.backgroundColor = '#FFFFFF';
+    }else{
+        e.target.style.backgroundColor = 'black';
+    }
+};
 
 function addGrid(num){
     for (let row = 0; row < num; row++){
@@ -37,9 +59,13 @@ askBtn.addEventListener('click', () => {
     sketchPad.textContent = "";
     gridAmount = amount;
     addGrid(amount);
+    console.log(currentState)
 });
 
 clear.addEventListener('click', () => {
     sketchPad.textContent = "";
     addGrid(gridAmount);
 });
+
+eraseBtn.addEventListener('click', () => changeState('erase'));
+blackBtn.addEventListener('click', () => changeState('black'));
